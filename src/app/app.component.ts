f8dcc823b6bed0f0a51533dcada719d3bd034cb1
João Ricardo Côre Dutra <jrdutra_@msn.com>
2024-02-22 16:29:46 -0300

    Alguns testes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,5 +11,20 @@ import { MenuTopoComponent } from './menu-topo/menu-topo.component'
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'ferramentas';
+  meuTitulo = 'Esse é meu titulo renderizado';
+  ingredientList = [
+    {name: 'noodles', quantity: 1},
+    {name: 'miso broth', quantity: 1},
+    {name: 'egg', quantity: 2},
+    {name: 'egg', quantity: 4},
+  ];
+
+  addItem(){
+    this.ingredientList.push({name: 'item', quantity: 1})
+  }
+
+  removeItem(){
+    this.ingredientList.pop();
+  }
+
 }
