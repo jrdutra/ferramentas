6739aca0ba19c73e6e11d4ea59937f719c8e16ff
João Ricardo Côre Dutra <jrdutra_@msn.com>
2024-02-27 15:50:45 -0300

    Responsividade

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -5,6 +5,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { Router } from '@angular/router';
 
+
 @Component({
   selector: 'app-menu',
   standalone: true,
