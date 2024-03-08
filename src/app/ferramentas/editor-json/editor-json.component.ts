2ed08f83a330b2cc45a8184b5cf8346132ec3878
João Ricardo Côre Dutra <jrdutra_@msn.com>
2024-03-08 12:18:51 -0300

    Layout do formatador de json

--- src/app/ferramentas/editor-json/editor-json.component.ts
+++ src/app/ferramentas/editor-json/editor-json.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-editor-json',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatCheckboxModule, FormsModule, MatInputModule, MatFormFieldModule],
   templateUrl: './editor-json.component.html',
   styleUrl: './editor-json.component.css'
 })
 export class EditorJsonComponent {
+
+  strFonte: string = '';
+  strResultado: string = '';
+
+  codificaTexto(){
+    
+  }
 
 }
