8455f8d37176faf15cf6947368d87f6ba7dba595
João Ricardo Côre Dutra <jrdutra_@msn.com>
2024-03-23 14:16:42 -0300

    Layout do conversor OCR pronto

--- src/app/ferramentas/conversor-imagem-texto-ocr/conversor-imagem-texto-ocr.component.ts
+++ src/app/ferramentas/conversor-imagem-texto-ocr/conversor-imagem-texto-ocr.component.ts
@@ -1,19 +1,47 @@
 import { Component } from '@angular/core';
 import { DataService } from '../../data.service';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-conversor-imagem-texto-ocr',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatCheckboxModule, FormsModule, MatInputModule, MatFormFieldModule, CommonModule],
   templateUrl: './conversor-imagem-texto-ocr.component.html',
   styleUrl: './conversor-imagem-texto-ocr.component.css'
 })
 export class ConversorImagemTextoOcrComponent {
 
-  constructor(private dataService: DataService) { }
+  selectedFile: File | null = null;
+
+  strTextoExtraido: string = "";
+
+  constructor(private dataService: DataService) { 
+
+  }
 
   ngOnInit(): void {
     this.dataService.setTituloAplicacao("Conversor OCR");
+  }
+
+
+  carregaArquivo(event: any): void {
+    if(event.target != null){
+      this.selectedFile = event.target.files[0];
+    }
+  }
+
+  executarOcr(): void {
+    // Aqui você pode enviar o arquivo para um serviço ou fazer o que for necessário com ele
+    console.log('Arquivo selecionado:', this.selectedFile);
+  }
+
+  getPreviewUrl(file: File): string {
+    return URL.createObjectURL(file);
   }
 
 }
