f38bfb606067c3c38b6111a9186e8f0202a1e21e
João Ricardo Côre Dutra <jrdutra_@msn.com>
2024-03-29 23:07:16 -0300

    Ferramenta de timestamp criada, faltando criar a descrição e instruções

--- src/app/ferramentas/unix-timestamp/unix-timestamp.component.ts
+++ src/app/ferramentas/unix-timestamp/unix-timestamp.component.ts
@@ -16,10 +16,48 @@ import { DataService } from '../../data.service';
 })
 export class UnixTimestampComponent {
 
-  constructor(private dataService: DataService) { }
+  dia: number = 0;
+  mes: number = 0;
+  ano: number = 0;
+  hora: number = 0;
+  minuto: number = 0;
+  segundo: number = 0;
+  unixTimestamp: number = 0;
+
+  data: Date = new Date();
+
+  constructor(private dataService: DataService) {
+    
+  }
 
   ngOnInit(): void {
     this.dataService.setTituloAplicacao("Unix Timestamp");
+    this.exibeDataHora();
+  }
+
+  atualizaDataHora(): void {
+    this.data = new Date(this.ano, this.mes - 1, this.dia, this.hora, this.minuto, this.segundo);
+    this.exibeDataHora();
+  }
+
+  atualizaTimestamp(): void {
+    this.data = new Date(this.unixTimestamp * 1000);;
+    this.exibeDataHora();
+  }
+
+  atualizaDataHoraAgora(): void{
+    this.data = new Date();
+    this.exibeDataHora();
+  }
+
+  exibeDataHora(): void {
+    this.dia = this.data.getDate();
+    this.mes = this.data.getMonth() + 1; // Os meses começam de 0, então adicionamos 1
+    this.ano = this.data.getFullYear();
+    this.hora = this.data.getHours();
+    this.minuto = this.data.getMinutes();
+    this.segundo = this.data.getSeconds();
+    this.unixTimestamp = Math.floor(this.data.getTime() / 1000); // Convertendo para timestamp UNIX em segundos
   }
 
 }
