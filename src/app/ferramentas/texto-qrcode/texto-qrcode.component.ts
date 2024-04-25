b43f787d13e53afd9872c172d4457d404716bed5
João Ricardo Côre Dutra <jrdutra.com.br@gmail.com>
2024-04-25 14:50:49 -0300

    Inclusão do Texto para Qrcode
    Inclusão do Texto para Qrcode

--- src/app/ferramentas/texto-qrcode/texto-qrcode.component.ts
+++ src/app/ferramentas/texto-qrcode/texto-qrcode.component.ts
@@ -1,21 +1,27 @@
 import { Component } from '@angular/core';
 import { DataService } from '../../data.service';
 import { QRCodeModule } from 'angularx-qrcode';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import {MatSliderModule} from '@angular/material/slider';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-texto-qrcode',
   standalone: true,
-  imports: [QRCodeModule],
+  imports: [QRCodeModule,MatSliderModule, MatFormFieldModule, MatInputModule, MatIconModule, MatButtonModule, FormsModule],
   templateUrl: './texto-qrcode.component.html',
   styleUrl: './texto-qrcode.component.css'
 })
 export class TextoQrcodeComponent {
-  text: string = '';
+  texto: string = 'www.google.com.br';
+  tamanho: number = 256;
 
   constructor(private dataService: DataService) { }
 
   ngOnInit(): void {
     this.dataService.setTituloAplicacao("Texto para QrCode");
-    this.text = 'Texto é uma produção, verbal ou não verbal, que se constitui com algum código, no intuito de comunicar algo a alguém, em determinado tempo e espaço. Sua definição ampla se deve ao fato de também abranger diversos formatos.Pode-se compreender o texto verbal, oral e escrito, como uma prática social que utiliza estruturas verbais, organizadas e caracterizadas por suas estruturas linguísticas e sua função social, com vistas a cumprir um papel pessoal ou coletivo na vida humana. O mesmo se aplica aos textos não verbais, também compreendidos como uma ação social, diferenciando-se somente em função das estruturas e códigos utilizados.';
   }
 }
