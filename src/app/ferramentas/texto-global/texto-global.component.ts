84979daad5089e905037965e6d982fc0073dadb1
João Ricardo Côre Dutra <jrdutra.com.br@gmail.com>
2024-08-18 16:36:26 -0300

    Atualizando a importação do socket para dinâmica por conta de problema no build

--- src/app/ferramentas/texto-global/texto-global.component.ts
+++ src/app/ferramentas/texto-global/texto-global.component.ts
@@ -1,11 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy} from '@angular/core';
 import { DataService } from '../../data.service';
 //import { SocketService } from '../../services/socket/socket.service';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
-
-import { io, Socket } from 'socket.io-client';
 
 @Component({
   selector: 'app-texto-global',
@@ -16,7 +14,7 @@ import { io, Socket } from 'socket.io-client';
 })
 export class TextoGlobalComponent {
 
-  // private socket: Socket;
+  private socket: any;
   private readonly url = 'https://compartilhadortexto-fuf5efeqfjhybte3.brazilsouth-01.azurewebsites.net';
   //private readonly url = 'http://localhost:3000';
 
@@ -24,43 +22,44 @@ export class TextoGlobalComponent {
   strCaminhoIndicadorConexao: string = './assets/light-red-icon.png'
   strTexto: string = ''
 
-  constructor(private dataService: DataService) {
-    // this.socket = io(this.url, {
-    //   rejectUnauthorized: false
-    // });
-  }
+  constructor(private dataService: DataService) {  }
 
-  ngOnInit(): void {
-    // this.socket.on('connect', () => {
-    //   console.log('Conectado ao servidor Socket.io');
-    //   this.strCaminhoIndicadorConexao = './assets/light-green-icon.png'
-    //   this.strStatusConexao = "Conectado"
-    // });
-    // this.socket.on('update', (data) => {
-    //   this.strTexto = data;
-    // });
+  async ngOnInit(): Promise<void> {
+    const { io } = await import('socket.io-client');
+    this.socket = io(this.url, {
+      rejectUnauthorized: false
+    });
+    this.socket.on('connect', () => {
+      console.log('Conectado ao servidor Socket.io');
+      this.strCaminhoIndicadorConexao = './assets/light-green-icon.png'
+      this.strStatusConexao = "Conectado"
+    });
 
-    // // Evento acionado quando a conexão for fechada
-    // this.socket.on('disconnect', () => {
-    //   this.strCaminhoIndicadorConexao = './assets/light-red-icon.png'
-    //   this.strStatusConexao = "Desconectado"
-    // });
+    this.socket.on('update', (data: string) => {
+      this.strTexto = data;
+    });
 
-    // // Evento acionado quando ocorre um erro
-    // this.socket.on('connect_error', (err) => {
-    //   this.strCaminhoIndicadorConexao = './assets/light-red-icon.png'
-    //   this.strStatusConexao = "Desconectado"
-    // });
+    // Evento acionado quando a conexão for fechada
+    this.socket.on('disconnect', () => {
+      this.strCaminhoIndicadorConexao = './assets/light-red-icon.png'
+      this.strStatusConexao = "Desconectado"
+    });
+
+    // Evento acionado quando ocorre um erro
+    this.socket.on('connect_error', (err: Error) => {
+      this.strCaminhoIndicadorConexao = './assets/light-red-icon.png'
+      this.strStatusConexao = "Desconectado"
+    });
   }
 
   ngOnDestroy(): void {
-    // if (this.socket) {
-    //   this.socket.disconnect();
-    // }
+    if (this.socket) {
+      this.socket.disconnect();
+    }
   }
 
   emiteTextoParaServidor() {
-    // this.socket.emit('updateTextoGlobal', this.strTexto);
+    this.socket.emit('updateTextoGlobal', this.strTexto);
   }
 
 }
